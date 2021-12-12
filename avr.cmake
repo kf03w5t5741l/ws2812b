e44793f06c52a739e76819b8fbb7dc0190c331f8
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2021-12-12 12:03:29 +0100

    Add cross-platform CMake fixes

--- avr.cmake
+++ avr.cmake
@@ -2,6 +2,8 @@
 # https://www.kuon.ch/post/2018-07-11-avr-cmake/
 
 set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
+
 set(MCU atmega328p)
 set(PROG_TYPE arduino)
 
@@ -13,7 +15,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
 
 add_compile_options(
     -mmcu=${MCU}
